1d03319e55b4b3b9138b43fedab2b4c4de6a49d0
이성희 <heeracle@naver.com>
2023-01-17 15:15:25 +0900

    first commit

---
+++ backend/focus/src/main/java/com/bb/focus/db/repository/CompanyAdminRepository.java
@@ -0,0 +1,10 @@
+package com.bb.focus.db.repository;
+
+import com.bb.focus.db.entity.CompanyAdmin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyAdminRepository extends JpaRepository<CompanyAdmin, Long> {
+
+}
---
+++ backend/focus/src/main/java/com/bb/focus/db/repository/CompanyAdminRepositorySupport.java
@@ -0,0 +1,12 @@
+package com.bb.focus.db.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CompanyAdminRepositorySupport {
+    /*
+    * @Autowired
+    * private JPAQueryFactory jpaQueryFactory;
+    * ...
+    * */
+}
