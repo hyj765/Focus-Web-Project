7dc82f1cda16abf411d35fc65d3d4782bf39e3d4
황영준 <hyj765@naver.com>
2023-01-25 11:13:31 +0900

    Update ServiceAdmin.java

--- backend/focus/src/main/java/com/bb/focus/db/entity/admin/ServiceAdmin.java
+++ backend/focus/src/main/java/com/bb/focus/db/entity/admin/ServiceAdmin.java
@@ -9,6 +9,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import org.hibernate.annotations.ColumnDefault;
 
