7c46fc32f39b3fd496cb8763d8f2d1a848823d03
IRyeong <dasliebeich7@gmail.com>
2023-01-27 16:37:24 +0900

    :sparkle: feat: redis 구현(진행중)

--- backend/focus/src/main/java/com/bb/focus/api/controller/AuthController.java
+++ backend/focus/src/main/java/com/bb/focus/api/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.bb.focus.api.controller;
 
 import com.bb.focus.api.request.UserLoginPostReq;
+import com.bb.focus.api.response.EvaluatorRes;
 import com.bb.focus.api.response.UserLoginPostRes;
 import com.bb.focus.api.service.ApplicantService;
 import com.bb.focus.api.service.CompanyAdminService;
 import com.bb.focus.api.service.EvaluatorService;
 import com.bb.focus.api.service.ServiceAdminService;
+import com.bb.focus.common.auth.FocusUserDetails;
 import com.bb.focus.common.model.response.BaseResponseBody;
 import com.bb.focus.common.util.JwtTokenUtil;
 import com.bb.focus.db.entity.admin.ServiceAdmin;
@@ -20,10 +22,14 @@ import io.swagger.annotations.ApiResponses;
 import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * 인증 관련 API 요청 처리를 위한 컨트롤러 정의.
@@ -125,5 +131,17 @@ public class AuthController {
       return true;
     }
     return false;
+  }
+
+  @PostMapping("/logout")
+  public ResponseEntity<?> logout(@ApiIgnore Authentication authentication) {
+
+    FocusUserDetails userDetails = (FocusUserDetails) authentication.getDetails();
+    System.out.println("logout :"+ authentication);
+    System.out.println(userDetails.toString());
+    String userId = userDetails.getUsername();
+    Evaluator evaluator = evaluatorService.getEvaluatorByUserId(userId);
+
+    return ResponseEntity.status(200).body(EvaluatorRes.of(evaluator));
   }
 }
