192eab39fbb001f2e13d25ff09fab60951d1e2c2
Jinwoo Park <zacinthepark@gmail.com>
2023-01-31 17:03:18 +0900

    front view settings

---
+++ frontend/vue-focus/src/TheHeader.vue
@@ -0,0 +1,54 @@
+<template>
+	<nav
+		class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900"
+	>
+		<div class="container flex flex-wrap items-center justify-between mx-auto">
+			<span
+				class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+				>Focus</span
+			>
+			<div class="hidden w-full md:block md:w-auto" id="navbar-default">
+				<ul
+					class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+				>
+					<li>
+						<RouterLink
+							to="/service"
+							class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+							aria-current="page"
+							>서비스관리자</RouterLink
+						>
+					</li>
+					<li>
+						<RouterLink
+							to="/corporate"
+							class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+							aria-current="page"
+							>기업관리자</RouterLink
+						>
+					</li>
+					<li>
+						<RouterLink
+							to="/evaluate"
+							class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+							aria-current="page"
+							>평가자</RouterLink
+						>
+					</li>
+					<li>
+						<RouterLink
+							to="/apply"
+							class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+							aria-current="page"
+							>지원자</RouterLink
+						>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
