1e7b6c9a3a653730e2cc57a528a061e352d07c35
이성희 <heeracle@naver.com>
2023-02-02 17:21:08 +0900

    :sparkle: feature: 면접일정 생성, 수정, 삭제 완료

--- backend/focus/src/main/java/com/bb/focus/api/service/ApplicantEvaluatorServiceImpl.java
+++ backend/focus/src/main/java/com/bb/focus/api/service/ApplicantEvaluatorServiceImpl.java
@@ -2,12 +2,17 @@ package com.bb.focus.api.service;
 
 import com.bb.focus.api.request.AddRemoveApplicantReq;
 import com.bb.focus.api.request.AddRemoveEvaluatorReq;
+import com.bb.focus.db.entity.applicant.Applicant;
+import com.bb.focus.db.entity.evaluation.EvaluationSheet;
 import com.bb.focus.db.entity.evaluator.Evaluator;
 import com.bb.focus.db.entity.helper.ApplicantEvaluator;
+import com.bb.focus.db.entity.interview.Interview;
 import com.bb.focus.db.entity.interview.InterviewRoom;
 import com.bb.focus.db.repository.ApplicantEvaluatorRepository;
+import com.bb.focus.db.repository.ApplicantRepository;
 import com.bb.focus.db.repository.EvaluatorRepository;
 import com.bb.focus.db.repository.InterviewRoomRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,11 +25,10 @@ public class ApplicantEvaluatorServiceImpl implements ApplicantEvaluatorService 
   private final ApplicantEvaluatorRepository applicantEvaluatorRepository;
   private final InterviewRoomRepository interviewRoomRepository;
   private final EvaluatorRepository evaluatorRepository;
+  private final ApplicantRepository applicantRepository;
 
   @Transactional
   public void mappingEvaluatorWithApplicants(AddRemoveEvaluatorReq addRemoveEvaluatorReq) {
-
-    ApplicantEvaluator applicantEvaluator = new ApplicantEvaluator();
 
     InterviewRoom interviewRoom = interviewRoomRepository
         .findById(addRemoveEvaluatorReq.getInterviewRoomId())
@@ -34,14 +38,58 @@ public class ApplicantEvaluatorServiceImpl implements ApplicantEvaluatorService 
         .findById(addRemoveEvaluatorReq.getEvaluatorId())
         .orElseThrow(IllegalArgumentException::new);
 
-    //applicant
-    //interview
-    //evaluationSheet
+    //interview : 면접(n차)
+    Interview interview = interviewRoom.getInterview();
 
+    //evaluationSheet : 평가지
+    EvaluationSheet evaluationSheet = interview.getEvaluationSheet();
+
+    //applicant
+    List<Long> applicantIds = applicantEvaluatorRepository.findApplicantIds(addRemoveEvaluatorReq.getInterviewRoomId());
+    for(Long applicantId : applicantIds){
+      Applicant applicant = applicantRepository.findById(applicantId).orElseThrow(IllegalArgumentException::new);
+
+      ApplicantEvaluator applicantEvaluator = new ApplicantEvaluator();
+      applicantEvaluator.setEvaluator(evaluator);
+      applicantEvaluator.setApplicant(applicant);
+      applicantEvaluator.setInterview(interview);
+      applicantEvaluator.setEvaluationSheet(evaluationSheet);
+      applicantEvaluator.setInterviewRoom(interviewRoom);
+
+      applicantEvaluatorRepository.save(applicantEvaluator);
+    }
   }
 
   @Transactional
   public void mappingApplicantWithEvaluators(AddRemoveApplicantReq addRemoveApplicantReq) {
 
+    InterviewRoom interviewRoom = interviewRoomRepository
+        .findById(addRemoveApplicantReq.getInterviewRoomId())
+        .orElseThrow(IllegalArgumentException::new);
+
+    Applicant applicant = applicantRepository
+        .findById(addRemoveApplicantReq.getApplicantId())
+        .orElseThrow(IllegalArgumentException::new);
+
+    //interview : 면접(n차)
+    Interview interview = interviewRoom.getInterview();
+
+    //evaluationSheet : 평가지
+    EvaluationSheet evaluationSheet = interview.getEvaluationSheet();
+
+    //evaluator
+    List<Long> evaluatorIds = applicantEvaluatorRepository.findEvaluatorIds(addRemoveApplicantReq.getInterviewRoomId());
+    for(Long evaluatorId : evaluatorIds){
+      Evaluator evaluator = evaluatorRepository.findById(evaluatorId).orElseThrow(IllegalArgumentException::new);
+
+      ApplicantEvaluator applicantEvaluator = new ApplicantEvaluator();
+      applicantEvaluator.setEvaluator(evaluator);
+      applicantEvaluator.setApplicant(applicant);
+      applicantEvaluator.setInterview(interview);
+      applicantEvaluator.setEvaluationSheet(evaluationSheet);
+      applicantEvaluator.setInterviewRoom(interviewRoom);
+
+      applicantEvaluatorRepository.save(applicantEvaluator);
+    }
   }
 }
--- backend/focus/src/main/java/com/bb/focus/db/repository/InterviewRoomCustomRepository.java
+++ backend/focus/src/main/java/com/bb/focus/db/repository/InterviewRoomCustomRepository.java
@@ -1,5 +1,8 @@
 package com.bb.focus.db.repository;
 
+import java.util.List;
+
 public interface InterviewRoomCustomRepository {
+
 
 }
