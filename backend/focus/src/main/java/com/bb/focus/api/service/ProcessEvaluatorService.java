1a668976b0acdc9e29ea232e86321ae3b7f3b42b
이성희 <heeracle@naver.com>
2023-02-02 13:05:01 +0900

    :sparkle: feature: 프로세스-평가자 테이블에 데이터 넣기

---
+++ backend/focus/src/main/java/com/bb/focus/api/service/ProcessEvaluatorService.java
@@ -0,0 +1,7 @@
+package com.bb.focus.api.service;
+
+public interface ProcessEvaluatorService {
+
+  public void saveProcessEvaluator(Long processId, Long evaluatorId);
+
+}
---
+++ backend/focus/src/main/java/com/bb/focus/db/repository/ProcessEvaluatorRepository.java
@@ -0,0 +1,8 @@
+package com.bb.focus.db.repository;
+
+import com.bb.focus.db.entity.helper.ProcessEvaluator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcessEvaluatorRepository extends JpaRepository<ProcessEvaluator, Long> {
+
+}
