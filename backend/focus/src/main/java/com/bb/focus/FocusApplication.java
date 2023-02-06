7be1489643cad229cdde2ed7a90ea746429fabdd
이성희 <heeracle@naver.com>
2023-02-06 09:38:54 +0900

    :sparkle: feature: /api 넣어봄

--- backend/focus/src/main/java/com/bb/focus/FocusApplication.java
+++ backend/focus/src/main/java/com/bb/focus/FocusApplication.java
@@ -3,9 +3,11 @@ package com.bb.focus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@RequestMapping("/api")
 public class FocusApplication {
 
 	public static void main(String[] args) {
