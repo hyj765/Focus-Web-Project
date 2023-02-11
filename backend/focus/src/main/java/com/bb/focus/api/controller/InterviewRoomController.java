67bb786013e1670b14bfecdd7c4b5ca8c688b142
황영준 <hyj765@naver.com>
2023-02-11 19:20:58 +0900

    Update InterviewRoomController.java

--- backend/focus/src/main/java/com/bb/focus/api/controller/InterviewRoomController.java
+++ backend/focus/src/main/java/com/bb/focus/api/controller/InterviewRoomController.java
@@ -38,7 +38,7 @@ public class InterviewRoomController {
   @ApiOperation(value = "면접실 코드번호 조회")
   @GetMapping("/enter/real")
   public ResponseEntity<?> getRealRoomCode(
-      @RequestParam(name="room-id") @ApiParam(value = "면접 일정 시퀀스 넘버", required = true) Long interviewRoomId) {
+      @RequestParam(name="roomId") @ApiParam(value = "면접 일정 시퀀스 넘버", required = true) Long interviewRoomId) {
 
     Optional<InterviewRoom> interviewRoom = interviewRoomService.findById(interviewRoomId);
     Room room = interviewRoom.get().getRoom();
