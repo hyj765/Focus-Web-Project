1db14b31ee66cd6183abddbc5c6ad6a98a454a54
IRyeong <dasliebeich7@gmail.com>
2023-02-13 16:38:17 +0900

    :sparkle: feat: 평가 기능 추가6

--- backend/focus/src/main/java/com/bb/focus/api/response/InterviewRoomRes.java
+++ backend/focus/src/main/java/com/bb/focus/api/response/InterviewRoomRes.java
@@ -57,6 +57,7 @@ public class InterviewRoomRes implements Comparable<InterviewRoomRes> {
     processName = interviewRoom.getProcessName();
     interviewRound = interviewRoom.getInterviewRound();
     realRoomCode = interviewRoom.getRoom().getRealCode();
+    applicantEvaluatorList = interviewRoom.getApplicantEvaluatorList();
   }
 
   public InterviewRoomRes of(InterviewRoom interviewRoom){
