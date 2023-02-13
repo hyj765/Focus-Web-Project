4099e86fef8d1e1ac67c9ed1c3ca94e38adef001
IRyeong <dasliebeich7@gmail.com>
2023-02-13 13:31:50 +0900

    :chore: fix: InterviewRoomController 원복

--- backend/focus/src/main/java/com/bb/focus/api/controller/InterviewRoomController.java
+++ backend/focus/src/main/java/com/bb/focus/api/controller/InterviewRoomController.java
@@ -26,7 +26,7 @@ public class InterviewRoomController {
   @ApiOperation(value = "대기실 코드번호 조회")
   @GetMapping("/enter/wait")
   public ResponseEntity<?> getWaitRoomCode(
-      @RequestBody @Valid @ApiParam(value = "면접 일정 시퀀스 넘버", required = true) Long interviewRoomId) {
+      @Valid @ApiParam(value = "면접 일정 시퀀스 넘버", required = true) Long interviewRoomId) {
 
     Optional<InterviewRoom> interviewRoom = interviewRoomService.findById(interviewRoomId);
     Room room = interviewRoom.get().getRoom();
@@ -36,9 +36,9 @@ public class InterviewRoomController {
   }
 
   @ApiOperation(value = "면접실 코드번호 조회")
-  @GetMapping("/enter/real/{room-id}")
+  @GetMapping("/enter/real")
   public ResponseEntity<?> getRealRoomCode(
-      @PathVariable(name="room-id") @ApiParam(value = "면접 일정 시퀀스 넘버", required = true) Long interviewRoomId) {
+      @Valid @ApiParam(value = "면접 일정 시퀀스 넘버", required = true) Long interviewRoomId) {
 
     Optional<InterviewRoom> interviewRoom = interviewRoomService.findById(interviewRoomId);
     Room room = interviewRoom.get().getRoom();
