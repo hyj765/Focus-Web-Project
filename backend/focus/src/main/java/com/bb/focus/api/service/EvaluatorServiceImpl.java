e8fa047125463620f4a7d15fb90eb54b1025aa72
이성희 <heeracle@naver.com>
2023-02-13 11:07:06 +0900

    :sparkle : feat: 비밀번호 특수문자 제외

--- backend/focus/src/main/java/com/bb/focus/api/service/EvaluatorServiceImpl.java
+++ backend/focus/src/main/java/com/bb/focus/api/service/EvaluatorServiceImpl.java
@@ -176,7 +176,7 @@ public class EvaluatorServiceImpl implements EvaluatorService{
     Random random = new Random();
 
     String generatedString = random.ints(leftLimit, rightLimit + 1)
-//        .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+        .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
         .limit(targetStringLength)
         .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
         .toString();
