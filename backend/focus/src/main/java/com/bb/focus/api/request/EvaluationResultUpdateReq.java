b84b1c72235cc5b1efe0715680467c8ca08dbe4d
IRyeong <dasliebeich7@gmail.com>
2023-02-14 10:45:43 +0900

    :sparkle: feat: 평가내역 수정 구현2

--- backend/focus/src/main/java/com/bb/focus/api/request/EvaluationResultUpdateReq.java
+++ backend/focus/src/main/java/com/bb/focus/api/request/EvaluationResultUpdateReq.java
@@ -13,11 +13,11 @@ import javax.validation.constraints.NotBlank;
 public class EvaluationResultUpdateReq {
 
   @ApiModelProperty(name="평가 결과 시퀀스 넘버")
-  Long EvaluationResultId;
+  Long evaluationResultId;
 
   @ApiModelProperty(name="평가 결과", example = "전문성이 부족해보임 자료구조에 대한 이해도가 부족함.") // 평가자가 쓴 메모
   @NotBlank(message = "평과 결과를 입력해주세요.")
-  String Content;
+  String content;
 
   @ApiModelProperty(name="평가 점수", example = "100")
   byte score;
