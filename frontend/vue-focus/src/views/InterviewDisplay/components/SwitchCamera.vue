bf95a59b5b31412b9466583a58703715c010e63f
Ming0Kim <minofficial13@gmail.com>
2023-02-14 09:11:38 +0900

    :sparkles: design: SwitchCamera

--- frontend/vue-focus/src/views/InterviewDisplay/components/SwitchCamera.vue
+++ frontend/vue-focus/src/views/InterviewDisplay/components/SwitchCamera.vue
@@ -1,73 +1,193 @@
 <template>
   <div>
     <div class="videoInput" style="display: flex; margin-left: 13%">
-      <video
-        class="myVideo"
-        autoplay
-        :poster="posterImgSrc"
-        style="display: flex"
-      ></video>
-      <img src="~@/assets/guide_image.jpg" alt="Guide_Image" />
+      <video class="myVideo" autoplay :poster="posterImgSrc"></video>
+      <!-- <img src="~@/assets/guide_image.jpg" alt="Guide_Image" /> -->
     </div>
-    <div class="text-center">
-      <button class="btn btn-secondary" @click="onClickCameraBtn">
-        <font-awesome-icon :icon="['fas', cameraIcon]" />
-      </button>
-
-      <button class="btn btn-secondary" @click="onClickMuteBtn">
-        <font-awesome-icon :icon="['fas', mutedIcon]" />
-      </button>
-
-      <button class="btn btn-secondary" @click="selectsOn = !selectsOn">
-        <img src="~@/assets/settings.png" alt="setting" class="setting" />
-        <!-- <font-awesome-icon :icon="['fas', 'cog']" /> -->
-      </button>
-
-      <div v-if="selectsOn" class="select-container my-2">
-        <select
-          id="cameraSelect"
-          v-model="deviceSetting.currentVideoId"
-          name="cameras"
-          class="form-select form-select-sm text-truncate"
-          @change="onChangeCamera"
+    <div class="flex flex-row justify-center">
+      <div>
+        <div
+          class="flex justify-center w-16 space-x-2 rounded"
+          :class="cameraBackground"
         >
-          <option value="0" disabled>카메라 선택</option>
-          <option
-            v-for="camera in cameraDevices"
-            :key="camera.deviceId"
-            :value="camera.deviceId"
-          >
-            {{ camera.label }}
-          </option>
-        </select>
-        <select
-          id="micSelect"
-          v-model="deviceSetting.currentAudioId"
-          name="mics"
-          class="form-select form-select-sm text-truncate"
-          @change="onChangeCamera"
+          <button @click="onClickCameraBtn" type="button">
+            <i :class="cameraIcon"></i>
+          </button>
+          <button @click="selectsOnCamera = !selectsOnCamera">
+            <i class="bx bx-chevron-down"></i>
+          </button>
+        </div>
+      </div>
+      <div>
+        <div
+          class="flex justify-center w-16 space-x-2 rounded"
+          :class="muteBackground"
         >
-          <option value="0" disabled>마이크 선택</option>
-          <option
-            v-for="mic in micDevices"
-            :key="mic.deviceId"
-            :value="mic.deviceId"
+          <button @click="onClickMuteBtn" type="button">
+            <i :class="mutedIcon"></i>
+          </button>
+          <button @click="selectsOn = !selectsOn">
+            <i class="bx bx-chevron-down"></i>
+          </button>
+        </div>
+      </div>
+
+      <div class="flex flex-col">
+        <button class="btn btn-secondary" @click="selectsOn = !selectsOn">
+          <img src="~@/assets/settings.png" alt="setting" class="w-6 setting" />
+          <!-- <font-awesome-icon :icon="['fas', 'cog']" /> -->
+        </button>
+        <div v-if="selectsOnCamera" class="fixed my-2 select-container w-72">
+          <Listbox
+            v-model="deviceSetting.currentVideoId"
+            id="cameraSelect"
+            name="cameras"
+            @change="onChangeCamera"
           >
-            {{ mic.label }}
-          </option>
-        </select>
+            <div class="relative mt-1">
+              <ListboxButton
+                class="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+              >
+                <span class="block truncate">카메라 선택</span>
+                <span
+                  class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
+                >
+                  <ChevronUpDownIcon
+                    class="w-5 h-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </ListboxButton>
+
+              <transition
+                leave-active-class="transition duration-100 ease-in"
+                leave-from-class="opacity-100"
+                leave-to-class="opacity-0"
+              >
+                <ListboxOptions
+                  class="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                >
+                  <ListboxOption
+                    v-slot="{ active, selected }"
+                    v-for="camera in cameraDevices"
+                    :key="camera.deviceId"
+                    :value="camera.deviceId"
+                    as="template"
+                  >
+                    <li
+                      :class="[
+                        active
+                          ? 'bg-amber-100 text-amber-900'
+                          : 'text-gray-900',
+                        'relative cursor-default select-none py-2 pl-10 pr-4',
+                      ]"
+                    >
+                      <span
+                        :class="[
+                          selected ? 'font-medium' : 'font-normal',
+                          'block truncate',
+                        ]"
+                        >{{ camera.label }}</span
+                      >
+                      <span
+                        v-if="selected"
+                        class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                      >
+                        <CheckIcon class="w-5 h-5" aria-hidden="true" />
+                      </span>
+                    </li>
+                  </ListboxOption>
+                </ListboxOptions>
+              </transition>
+            </div>
+          </Listbox>
+        </div>
+        <div v-if="selectsOn" class="fixed my-2 select-container w-72">
+          <Listbox
+            v-model="deviceSetting.currentAudioId"
+            id="micSelect"
+            name="mics"
+            @change="onChangeCamera"
+          >
+            <div class="relative mt-1">
+              <ListboxButton
+                class="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+              >
+                <span class="block truncate">마이크 선택</span>
+                <span
+                  class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
+                >
+                  <ChevronUpDownIcon
+                    class="w-5 h-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </ListboxButton>
+
+              <transition
+                leave-active-class="transition duration-100 ease-in"
+                leave-from-class="opacity-100"
+                leave-to-class="opacity-0"
+              >
+                <ListboxOptions
+                  class="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                >
+                  <ListboxOption
+                    v-slot="{ active, selected }"
+                    v-for="mic in micDevices"
+                    :key="mic.deviceId"
+                    :value="mic.deviceId"
+                    as="template"
+                  >
+                    <li
+                      :class="[
+                        active
+                          ? 'bg-amber-100 text-amber-900'
+                          : 'text-gray-900',
+                        'relative cursor-default select-none py-2 pl-10 pr-4',
+                      ]"
+                    >
+                      <span
+                        :class="[
+                          selected ? 'font-medium' : 'font-normal',
+                          'block truncate',
+                        ]"
+                        >{{ mic.label }}</span
+                      >
+                      <span
+                        v-if="selected"
+                        class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                      >
+                        <CheckIcon class="w-5 h-5" aria-hidden="true" />
+                      </span>
+                    </li>
+                  </ListboxOption>
+                </ListboxOptions>
+              </transition>
+            </div>
+          </Listbox>
+        </div>
       </div>
     </div>
   </div>
 </template>
+<script setup>
+import { ref } from 'vue';
+import {
+  Listbox,
+  ListboxLabel,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid';
+</script>
 <script>
 import notifications from '@/composables/notifications';
-
 const { notifyDanger } = notifications();
 
 export default {
   name: 'StudyDetailCamera',
-  components: {},
   data() {
     return {
       myStream: null,
@@ -77,6 +197,7 @@ export default {
       micDevices: [],
       initMic: 0,
       selectsOn: false,
+      selectsOnCamera: false,
       deviceSetting: {
         currentVideoId: '',
         currentAudioId: '',
@@ -88,10 +209,22 @@ export default {
   },
   computed: {
     mutedIcon() {
-      return this.deviceSetting.isMuted ? 'microphone-slash' : 'microphone';
+      return this.deviceSetting.isMuted
+        ? 'bx bx-volume-mute'
+        : 'bx bx-volume-full';
+    },
+    muteBackground() {
+      return this.deviceSetting.isMuted
+        ? 'inline-block leading-normal text-white uppercase transition duration-150 ease-in-out bg-red-600 rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg w-9 h-9'
+        : 'inline-block leading-normal text-white uppercase transition duration-150 ease-in-out bg-indigo-600 rounded-full shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg w-9 h-9';
     },
     cameraIcon() {
-      return this.deviceSetting.isCameraOn ? 'video' : 'video-slash';
+      return this.deviceSetting.isCameraOn ? 'bx bx-video' : 'bx bx-video-off';
+    },
+    cameraBackground() {
+      return this.deviceSetting.isCameraOn
+        ? 'inline-block leading-normal text-white uppercase transition duration-150 ease-in-out bg-indigo-600 rounded-full shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg w-9 h-9'
+        : 'inline-block leading-normal text-white uppercase transition duration-150 ease-in-out bg-red-600 rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg w-9 h-9';
     },
   },
   watch: {
@@ -245,30 +378,16 @@ video {
   margin-top: 0;
   border-radius: 1rem;
   justify-content: center;
-  width: 50%;
+  width: 70%;
   aspect-ratio: 30 / 19;
   overflow: hidden;
   object-fit: cover;
   border-radius: 1rem;
+  transform: rotateY(180deg);
 }
 
 videoInput {
   width: auto;
   height: auto;
-}
-
-button {
-  border-radius: 0%;
-  height: 5rem;
-  width: 5rem;
-  margin: 0 0.5rem 0px;
-  color: black;
-}
-
-.setting {
-  margin-left: 40%;
-  width: 14px;
-  height: 14px;
-  object-fit: cover;
 }
 </style>
