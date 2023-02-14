27a1a7f0d848711d464fd1f44a9a4b507084d52f
황영준 <hyj765@naver.com>
2023-02-14 22:56:14 +0900

    Revert "Merge branch 'feature/applicantDetail' into 'back'"
    This reverts merge request !127

--- frontend/vue-focus/src/views/ApplicantHomeView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h2>Applicant</h2>
-  </div>
-</template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
--- frontend/vue-focus/src/views/EvaluatorSetting.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>EvaluatorSetting</div>
-</template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
