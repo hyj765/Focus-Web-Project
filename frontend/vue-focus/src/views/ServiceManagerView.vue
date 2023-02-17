9d0b0d5ac65794015f37cc8b89392c6dc8d89e2c
Jinwoo Park <zacinthepark@gmail.com>
2023-02-17 13:46:07 +0900

    fix merge conflicts

---
+++ frontend/vue-focus/src/views/ServiceManagerView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <TheSideBar />
+  </div>
+</template>
+
+<script setup>
+import TheSideBar from '@/layouts/TheSideBar.vue';
+</script>
+
+<style lang="scss" scoped></style>
