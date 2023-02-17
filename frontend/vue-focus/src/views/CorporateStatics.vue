e05ff26e33895b4479ec415f57efa061f8383ea4
Jinwoo Park <zacinthepark@gmail.com>
2023-02-17 14:08:33 +0900

    fix merge conflicts

--- frontend/vue-focus/src/views/CorporateStatics.vue
+++ frontend/vue-focus/src/views/CorporateStatics.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>CorporateStatics</div>
+  <div>
+    <nav class="flex flex-wrap justify-between p-8 text-gray-800">
+      <h1 class="font-bold">네이버 님, 안녕하세요</h1>
+      <h3 class="font-bold text-gray-500">Statics</h3>
+    </nav>
+    <p class="px-10 text-2xl font-medium">합격자 통계</p>
+    <p class="px-10 pt-1 text-gray-700 font-regular">
+      합격자의 통계를 확인하세요
+    </p>
+    <div class="p-10">
+      <div class="overflow-hidden bg-white shadow sm:rounded-lg">
+        <div class="px-4 py-5 border-b-2 sm:px-6">
+          <h3 class="text-lg font-medium leading-6 text-gray-900">
+            두나무 개발자
+          </h3>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
