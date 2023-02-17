7b3fa97daa9e8ecc3f890ce46cf32b47dec638be
Jinwoo Park <zacinthepark@gmail.com>
2023-02-17 14:02:17 +0900

    fix merge conflicts

--- frontend/vue-focus/src/views/ServiceNotice.vue
+++ frontend/vue-focus/src/views/ServiceNotice.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
     <h1 class="text-2xl text-red-900">Notice</h1>
+    <ul>
+      <li v-for="notice in notices" :key="notice">{{ notice.title }}</li>
+    </ul>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const notices = ref([]);
+
+onMounted(() => {
+  axios
+    .get('http://localhost:3000/notices')
+    .then(({ data }) => {
+      notices.value = data.notices.notices;
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+</script>
 
 <style lang="scss" scoped></style>
