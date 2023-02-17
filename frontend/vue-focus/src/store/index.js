d1e9f629199d34a098937657095e32a85cbfb57a
Jinwoo Park <zacinthepark@gmail.com>
2023-02-17 15:13:47 +0900

    fix merge conflicts

--- frontend/vue-focus/src/store/index.js
+++ frontend/vue-focus/src/store/index.js
@@ -18,7 +18,7 @@ export default createStore({
       localStorage.setItem('user', JSON.stringify(userData));
       axios.defaults.headers.common[
         'Authorization'
-      ] = `Bearer ${userData.token}`;
+      ] = `Bearer ${userData.accessToken}`;
       console.log(
         'axios headers: ',
         axios.defaults.headers.common['Authorization'],
@@ -28,22 +28,18 @@ export default createStore({
       state.user = null;
       localStorage.removeItem('user');
       axios.defaults.headers.common['Authorization'] = null;
-
-      // 더 좋은 코드?
-      // localStorage.removeItem('user')
-      // location.reload()
     },
   },
   actions: {
     login({ commit }, credentials) {
-      console.log('LOGIN ACTIONS', credentials);
       return axios
         .post(`${BASE_URL}/auth/login`, credentials)
         .then(({ data }) => {
-          console.log('user data is: ', data);
-          // commit('SET_USER_DATA', data);
-          // console.log('state.user: ', this.state.user);
-          // console.log('getters.loggedIn', this.getters.loggedIn);
+          commit('SET_USER_DATA', data);
+          console.log('user: ', this.state.user);
+        })
+        .catch(err => {
+          console.log('login request failed! ', err.message);
         });
     },
     logout({ commit }) {
