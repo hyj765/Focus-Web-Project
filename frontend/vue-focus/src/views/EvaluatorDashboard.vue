6dd0760a3b61e811619410cd556b35246092b3a3
Jinwoo Park <zacinthepark@gmail.com>
2023-02-17 15:33:50 +0900

    fix merge conflicts

--- frontend/vue-focus/src/views/EvaluatorDashboard.vue
+++ frontend/vue-focus/src/views/EvaluatorDashboard.vue
@@ -30,7 +30,7 @@
           <div class="flex items-center justify-center">
             <button
               type="button"
-              class="place-content-center inline-block rounded bg-indigo-500 px-6 py-2.5 text-md font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg"
+              class="place-content-center inline-block rounded bg-indigo-500 mx-3 px-6 py-2.5 text-md font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg"
             >
               입장
             </button>
@@ -104,7 +104,9 @@
                   <td
                     class="px-6 py-4 text-lg font-light text-gray-900 whitespace-nowrap"
                   >
-                    3차
+                    <div class="mx-10">
+                      {{ item.interviewRound }}
+                    </div>
                   </td>
                   <td
                     class="px-6 py-4 text-lg font-light text-gray-900 whitespace-nowrap"
@@ -149,6 +151,52 @@ import EvaluateHomeRecentlySchedule from './EvaluateHomeRecentlySchedule.vue';
 import EvaluateHomeSchedule from './EvaluateHomeSchedule.vue';
 
 const goSettingRoom = () => router.push({ name: 'InterviewRoom' });
+
+const name = ref('');
+const startTime = ref('');
+const endTime = ref('');
+
+const BASE_URL = 'https://i8a106.p.ssafy.io/api';
+
+const scheduled = ref(null);
+const scheduledList = ref(null);
+
+const getRecentlySchedule = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  axios
+    .get(`${BASE_URL}/evaluators/next`, {
+      headers: {
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    })
+    .then(res => {
+      scheduled.value = res;
+      console.log(scheduled.value.data.name);
+      name.value = scheduled.value.data.name;
+      startTime.value = scheduled.value.data.startTime;
+      endTime.value = scheduled.value.data.endTime;
+    });
+};
+
+const getScheduleList = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  axios
+    .get(`${BASE_URL}/evaluators/list`, {
+      headers: {
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    })
+    .then(res => {
+      console.log('res.data: ', res.data);
+      scheduledList.value = res.data['2023-02-17'];
+      console.log(scheduledList.value);
+    });
+};
+
+onMounted(() => {
+  getRecentlySchedule();
+  getScheduleList();
+});
 </script>
 
 <style lang="scss" scoped></style>
